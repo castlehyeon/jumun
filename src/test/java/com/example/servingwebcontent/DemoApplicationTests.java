965c3ee4fcf66b953cd6dfc2fad9b30983ae5353
castlehyeon <qnftktls118@naver.com>
2022-08-14 23:53:30 +0900

    h2사용을 위한 의존성 코드 추가

--- src/test/java/com/example/servingwebcontent/DemoApplicationTests.java
+++ src/test/java/com/example/servingwebcontent/DemoApplicationTests.java
@@ -23,8 +23,9 @@ class DemoApplicationTests {
 
 
 	//단위테스트 1번: getHello함수를 실행했을 때, 다음과 같은 문자열을 반환받을 수 있는가?
-	// MOCKMVC를 통해 디스패치서블렛에게 http요청을 보낸다. https://velog.io/@jkijki12/Spring-MockMvc
-	//톰캣 실행할 필요없이 간단하게 웹환경을 만들어주는 것 같다.o
+	//MOCKMVC를 통해 디스패치서블렛에게 http요청을 보낸다. https://velog.io/@jkijki12/Spring-MockMvc
+	//톰캣 실행할 필요없이 간단하게 웹환경을 만들어주는 것 같다.
+	//json 통신할 때 필요할 것 같은데.
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
